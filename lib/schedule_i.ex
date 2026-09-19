c1b9e70076bc3cc83a16d2c04ad7234d86c72615
Roger Oglesby <development@essda.co.uk>
2026-09-19 19:25:53 +0100

    Initial Mix project

---
+++ lib/schedule_i.ex
@@ -0,0 +1,18 @@
+defmodule ScheduleI do
+  @moduledoc """
+  Documentation for `ScheduleI`.
+  """
+
+  @doc """
+  Hello world.
+
+  ## Examples
+
+      iex> ScheduleI.hello()
+      :world
+
+  """
+  def hello do
+    :world
+  end
+end
---
+++ test/schedule_i_test.exs
@@ -0,0 +1,8 @@
+defmodule ScheduleITest do
+  use ExUnit.Case
+  doctest ScheduleI
+
+  test "greets the world" do
+    assert ScheduleI.hello() == :world
+  end
+end
